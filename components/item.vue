83e2278810c30d7ce8d88d73ffa6d68440aee735
Radu <radu.bordei89@gmail.com>
2024-10-03 16:12:18 +0300

    fix

--- components/item.vue
+++ components/item.vue
@@ -3,7 +3,7 @@
         <div class="flex-1 items-center justify-center">
           <p class="text-xs font-medium text-gray-900 pb-1 px-1 hover:underline cursor-pointer h-[55px]" :title="item?.title">{{ (item?.title || '').substring(36, item?.title) + '…' }}</p>
           <p class="text-sm font-bold text-gray-900">{{ $currencyFormat(item.price, 'fr-FR', 'EUR') }}</p>
-          <p class="text-xs font-normal text-gray-900" v-if="item.area && item.price">{{ parseFloat(item?.price / parseFloat(new String(item?.area).match(/\d+(\.\d+)?/))).toFixed(2) }} EUR<span class="text-gray-800"> / m2</span></p>
+          <p class="text-[11px] font-normal text-gray-900" v-if="item.area && item.price">{{ parseFloat(item?.price / parseFloat(new String(item?.area).match(/\d+(\.\d+)?/))).toFixed(0) }} EUR<span class="text-gray-800"> / m2</span></p>
         </div>
         <img class="h-[5.5rem] w-[5.5rem] flex-shrink-0 rounded-xl cursor-pointer" :src="item?.images?.[0]" alt="" />
       </div>
