0408ed651380dd9e6d8166002c0ffacf467214c5
Radu <radu.bordei89@gmail.com>
2024-11-25 16:27:57 +0200

    fix

--- api/map.js
+++ api/map.js
@@ -1,9 +1,9 @@
 import { useCustomFetch } from '~/composables/useCustomFetch'
 
-const fetchNearestPlaces = async (lat, lon, amenity) => {
+const fetchNearestPlaces = async (lat, lon, amenity, distance_in_meters = 5000) => {
   const { data, error } = await useCustomFetch(`/map/nearest`, {
     method: 'POST',
-    body: JSON.stringify({ lat, lon, amenity }),
+    body: JSON.stringify({ lat, lon, amenity, distance_in_meters }),
     headers: {
       'Content-Type': 'application/json',
     },
