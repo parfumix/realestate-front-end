86a7cc417f2e8df0398aee3570f19ce6ff46f711
Radu <radu.bordei89@gmail.com>
2024-11-27 16:08:48 +0200

    fix

--- components/modals/real-estate-chat.vue
+++ components/modals/real-estate-chat.vue
@@ -14,7 +14,7 @@
             <div>
                 <div v-if="!defaultThreadMessages.length" class="border-b border-gray-200 mx-auto flex justify-center">
                     <nav class="-mb-px flex space-x-[12px]">
-                        <a v-for="(tab, index) in tabs" :key="tab.title" class="cursor-pointer py-4 px-1 flex items-center" @click="selectTab(tab.slug)" :class="{ 'border-b border-gray-500 text-blue-500': activeTab === index }">
+                        <a v-for="(tab, index) in tabs.filter(el => el.visible)" :key="tab.title" class="cursor-pointer py-4 px-1 flex items-center" @click="selectTab(tab.slug)" :class="{ 'border-b border-gray-500 text-blue-500': activeTab === index }">
                             <span v-html="tab.icon" :class="[{'text-gray-800': tab.slug==activeTab, 'text-gray-500': tab.slug!=activeTab}, 'pr-2']"></span>
                             <span v-html="tab.title" :class="[{'text-gray-800 font-medium hover:text-gray-900': tab.slug==activeTab, 'text-gray-500 font-medium hover:text-gray-700': tab.slug!=activeTab}, 'whitespace-nowrap text-sm']"></span>
                         </a>
@@ -56,11 +56,13 @@ const tabs = ref([
     {
         title: "Sunt cumpărător",
         slug: 'buyer',
+        visible: props.item.transaction_type == 'for-sale',
         icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-500"><path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>',
     },
     {
         title: "Sunt investor",
         slug: 'investor',
+        visible: props.item.transaction_type == 'for-sale',
         icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-500"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" /></svg>',
     },
 ]);
