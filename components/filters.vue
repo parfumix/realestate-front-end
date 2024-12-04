a69e23574fbf9d091714ddecb54e39ddbe0312e2
Radu <radu.bordei89@gmail.com>
2024-12-04 20:13:04 +0200

    rm unused code

--- components/filters.vue
+++ components/filters.vue
@@ -131,40 +131,6 @@
           </div>
         </div>
       </div>
-
-      <!-- Active filters -->
-      <!-- <div class="bg-gray-100 py-1">
-        <div class="mx-auto max-w-7xl px-4 sm:flex sm:justify-between sm:items-center sm:px-6 lg:px-8 min-h-10">
-          <div class="sm:flex sm:items-center sm:px-6 lg:px-8 min-h-10">
-            <h3 class="text-sm font-medium text-gray-500">
-              Filtre
-              <span class="sr-only">, active</span>
-            </h3>
-
-            <div class="hidden h-5 w-px bg-gray-300 sm:ml-4 sm:block"></div>
-
-            <div class="mt-2 sm:ml-4 sm:mt-0">
-              <div class="-m-1 flex flex-wrap items-center">
-                <span v-for="(activeFilter, index) in filterStore.activeFilters" :key="index" class="flex flex-row">
-                  <span v-for="filter in activeFilter" :key="`${index}-sub`"
-                    class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-white py-1.5 pl-3 pr-2 text-sm font-medium text-gray-900">
-                    <span>{{ filters.find(el => el.id == index)?.['options'].find(el => el.value == filter)?.label
-                      }}</span>
-                    <button type="button" @click="toggleFilter(index, filter)"
-                      class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-gray-400 hover:bg-gray-200 hover:text-gray-500">
-                      <span class="sr-only">Remove filter for {{ filter }}</span>
-                      <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
-                        <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
-                      </svg>
-                    </button>
-                  </span>
-
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> -->
     </section>
   </div>
 </template>
