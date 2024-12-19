84c7070c220c6ad0b5d24300495c8fd068cffdf4
Radu <radu.bordei89@gmail.com>
2024-12-19 15:50:44 +0200

    adding error message

--- components/form/textarea.vue
+++ components/form/textarea.vue
@@ -14,6 +14,7 @@
         :placeholder="placeholder"
         :class="[
           'block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6',
+          error ? 'text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500' : 'text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-500',
           borderColor,
           textColor,
           focusColor
@@ -22,6 +23,10 @@
         :maxlength="maxLength"
         @input="e => handleInput(e.target.value)"
       />
+
+      <p v-if="error" :id="`${name}-error`" class="mt-2 text-sm text-red-600">
+        {{ error }}
+      </p>
       <div class="mt-1 text-sm text-gray-500">
         {{ characterCount }} / {{ maxLength }}
       </div>
@@ -71,6 +76,10 @@ defineProps({
     type: Number,
     default: 200, // Default character limit
   },
+  error: {
+      type: String,
+      default: ''
+  }
 })
 
 const model = defineModel()
