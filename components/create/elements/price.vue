bfaba6a4d89b16ab73e9786e0ccb5ebe10cda242
Radu <radu.bordei89@gmail.com>
2024-12-24 20:12:02 +0200

    fix

--- components/create/elements/price.vue
+++ components/create/elements/price.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 
 const { value, errorMessage } = useField(() => props.name, yup
     .number()
-    .typeError('Prețul trebuie să fie un număr valid') // Error for non-numeric input
+    .typeError('Prețul trebuie să fie un număr') // Error for non-numeric input
     .positive('Prețul trebuie să fie mai mare decât 0') // Enforce positive values
     .required('Prețul este obligatoriu') // Mark as required
 );
