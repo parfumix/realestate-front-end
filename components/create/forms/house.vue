59d3dd78c48fc77a16bcd56fee41c482b595365a
Radu <radu.bordei89@gmail.com>
2024-12-24 17:11:20 +0200

    rm loor

--- components/create/forms/house.vue
+++ components/create/forms/house.vue
@@ -1,7 +1,6 @@
 <template>
     <CreateElementsTotalArea :title="fieldsMeta['totalArea'].long" name="totalArea" />
     <CreateElementsSurface :title="fieldsMeta['surface'].long" name="surface" />
-    <CreateElementsFloor :title="fieldsMeta['floor'].long" name="floor" />
     <CreateElementsLocation :title="fieldsMeta['location'].long" :description="fieldsMeta['location'].description" name="location" />
 </template>
 
