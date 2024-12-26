4529e2b946d5942f70c4bc3be940329f0b2af11a
Radu <radu.bordei89@gmail.com>
2024-12-26 15:37:58 +0200

    improve input element by adding right actions

--- components/form/input.vue
+++ components/form/input.vue
@@ -3,27 +3,30 @@
     <label :for="id" class="block text-sm font-medium leading-6 text-gray-900">
       <span v-if="required" class="text-red-600">*</span> {{ label }}
     </label>
-    <div class="relative mt-2 rounded-md shadow-sm">
-      <div class="absolute inset-y-0 left-0 flex items-center">
-        <slot name="prefix"></slot>
+    <div class="mt-2 flex rounded-md shadow-sm">
+      <div class="relative flex flex-grow items-stretch focus-within:z-10">
+        <div class="absolute inset-y-0 left-0 flex items-center">
+          <slot name="prefix"></slot>
+        </div>
+        <input
+          :type="type"
+          :name="name"
+          :id="id"
+          :class="[
+            inputClass,
+            error ? 'ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-1 focus:ring-inset focus:ring-red-500' : ' ring-1 ring-inset ring-gray-300 focus:ring-1 focus:ring-inset focus:ring-gray-400'
+          ]"
+          :placeholder="placeholder"
+          :value="model"
+          :aria-invalid="!!error"
+          :aria-describedby="error ? `${name}-error` : null"
+          @input="e => handleInput(e.target.value)"
+        />
+        <div v-if="error" class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+          <ExclamationCircleIcon class="h-5 w-5 text-red-500" aria-hidden="true" />
+        </div>
       </div>
-      <input
-        :type="type"
-        :name="name"
-        :id="id"
-        :class="[
-          inputClass,
-          error ? 'ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-1 focus:ring-inset focus:ring-red-500' : ' ring-1 ring-inset ring-gray-300 focus:ring-1 focus:ring-inset focus:ring-gray-400'
-        ]"
-        :placeholder="placeholder"
-        :value="model"
-        :aria-invalid="!!error"
-        :aria-describedby="error ? `${name}-error` : null"
-        @input="e => handleInput(e.target.value)"
-      />
-      <div v-if="error" class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-        <ExclamationCircleIcon class="h-5 w-5 text-red-500" aria-hidden="true" />
-      </div>
+      <slot name="actions"></slot>
     </div>
     <p v-if="error" :id="`${name}-error`" class="mt-2 text-sm text-red-600">
       {{ error }}
