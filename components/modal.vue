78306923dc726810d29cf481b660fe1d7a4e44c7
Radu <radu.bordei89@gmail.com>
2024-12-27 17:46:00 +0200

    align modal vertically

--- components/modal.vue
+++ components/modal.vue
@@ -7,7 +7,7 @@
             </TransitionChild>
 
             <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-                <div class="flex items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div class="flex items-center min-h-screen justify-center p-4 text-center sm:items-center sm:p-0">
                     <TransitionChild as="template" enter="ease-out duration-300"
                         enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
