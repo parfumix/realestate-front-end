7ab41aeaedb08a1a955ef648f3082a28a7c7dff7
Radu <radu.bordei89@gmail.com>
2024-12-27 15:24:39 +0200

    adding modal props

--- stores/modals.js
+++ stores/modals.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { ref, defineAsyncComponent } from 'vue';
+import { ref, shallowRef, defineAsyncComponent } from 'vue';
 
 export const useModalStore = defineStore('modalStore', () => {
   // Modal visibility state
@@ -8,9 +8,13 @@ export const useModalStore = defineStore('modalStore', () => {
   // Store the component to render
   const modalComponent = shallowRef(null);
 
-  // Open the modal and pass the component to be rendered
-  const openModal = (component) => {
+  // Store the arguments to pass to the modal
+  const modalProps = ref(null);
+
+  // Open the modal, pass the component to be rendered, and any arguments
+  const openModal = (component, props = null) => {
     modalComponent.value = defineAsyncComponent(() => Promise.resolve(component));
+    modalProps.value = props;
     isModalVisible.value = true;
   };
 
@@ -18,11 +22,13 @@ export const useModalStore = defineStore('modalStore', () => {
   const closeModal = () => {
     isModalVisible.value = false;
     modalComponent.value = null;
+    modalProps.value = null;
   };
 
   return {
     isModalVisible,
     modalComponent,
+    modalProps,
     openModal,
     closeModal,
   };
