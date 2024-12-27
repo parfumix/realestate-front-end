a4515d7b1839d4c8a2397b0c3bacb2ca88a20fe9
Radu <radu.bordei89@gmail.com>
2024-12-27 15:43:15 +0200

    improve modal store

--- stores/modals.js
+++ stores/modals.js
@@ -8,21 +8,45 @@ export const useModalStore = defineStore('modalStore', () => {
   // Store the component to render
   const modalComponent = shallowRef(null);
 
-  // Store the arguments to pass to the modal
+  // Store the props to pass to the modal
   const modalProps = ref(null);
 
-  // Open the modal, pass the component to be rendered, and any arguments
-  const openModal = (component, props = null) => {
+  // Store success and failure callbacks
+  const onSuccess = ref(null);
+  const onFailure = ref(null);
+
+  // Open the modal, passing the component, props, and callbacks
+  const openModal = (component, props = null, successCallback = null, failureCallback = null) => {
     modalComponent.value = defineAsyncComponent(() => Promise.resolve(component));
     modalProps.value = props;
+    onSuccess.value = successCallback;
+    onFailure.value = failureCallback;
     isModalVisible.value = true;
   };
 
-  // Close the modal
+  // Trigger the success callback and close the modal
+  const success = (result) => {
+    if (onSuccess.value) {
+      onSuccess.value(result);
+    }
+    closeModal();
+  };
+
+  // Trigger the failure callback and close the modal
+  const fail = (error) => {
+    if (onFailure.value) {
+      onFailure.value(error);
+    }
+    closeModal();
+  };
+
+  // Close the modal and reset state
   const closeModal = () => {
     isModalVisible.value = false;
     modalComponent.value = null;
     modalProps.value = null;
+    onSuccess.value = null;
+    onFailure.value = null;
   };
 
   return {
@@ -30,6 +54,8 @@ export const useModalStore = defineStore('modalStore', () => {
     modalComponent,
     modalProps,
     openModal,
+    success,
+    fail,
     closeModal,
   };
 });
