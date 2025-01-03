0e98901473c77597afc21a75ca1ccc5fcae6339f
Radu <radu.bordei89@gmail.com>
2025-01-03 15:26:35 +0200

    adding propertyService composable

---
+++ composables/usePropertiesService.js
@@ -0,0 +1,84 @@
+import { useCustomFetch } from '~/composables/useCustomFetch';
+
+export const usePropertyService = () => {
+    
+    const getAllProperties = async () => {
+        try {
+            const { data, error } = await useCustomFetch('/properties', {
+                method: 'GET',
+            });
+
+            if (error.value) throw new Error(error.value?.data?.message || 'Failed to fetch properties');
+
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const getPropertyBySlug = async (slug) => {
+        try {
+            const { data, error } = await useCustomFetch(`/properties/${slug}`, {
+                method: 'GET',
+            });
+
+            if (error.value) throw new Error(error.value?.data?.message || 'Failed to fetch property');
+
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const createProperty = async (property) => {
+        try {
+            const { data, error } = await useCustomFetch('/properties', {
+                method: 'POST',
+                body: property,
+            });
+
+            if (error.value) throw new Error(error.value?.data?.message || 'Failed to create property');
+
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const updateProperty = async (slug, property) => {
+        try {
+            const { data, error } = await useCustomFetch(`/properties/${slug}`, {
+                method: 'PUT',
+                body: property,
+            });
+
+            if (error.value) throw new Error(error.value?.data?.message || 'Failed to update property');
+
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const deletePropertyBySlug = async (slug) => {
+        try {
+            const { data, error } = await useCustomFetch(`/properties/${slug}`, {
+                method: 'DELETE',
+            });
+
+            if (error.value) throw new Error(error.value?.data?.message || 'Failed to delete property');
+
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    return {
+        getAllProperties,
+        getPropertyBySlug,
+        createProperty,
+        updateProperty,
+        deletePropertyBySlug,
+    }
+}
