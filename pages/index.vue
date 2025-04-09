a62717fb8ec7ddc0c7875b95073cf32815e843a3
Radu <radu.bordei89@gmail.com>
2025-04-09 19:42:43 +0300

    fix z-index

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@
     </div>
 
     <!-- Chatbox at the bottom, responsive -->
-    <div class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full lg:w-2/6 px-4 lg:px-0">
+    <div class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full lg:w-2/6 px-4 lg:px-0 z-[500]">
       <Chat 
         @submit="handleSendMessage" 
         @resetActiveMessage="handleResetActiveMessage" 
