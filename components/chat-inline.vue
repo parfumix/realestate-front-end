ad58f4adad46a8f9b63102cd01e5b5c718eb56f7
Radu <radu.bordei89@gmail.com>
2025-05-06 00:14:31 +0300

    fixes

--- components/chat-inline.vue
+++ components/chat-inline.vue
@@ -6,9 +6,20 @@
                 <input v-model="message" @focus="inputIsFocused=true"
                     @keyup.enter="() => handleSendMessage(message, false)" type="text" placeholder="Scrie ce cauți..."
                     class="py-2 px-4 focus:ring-0 border-0 shadow-md w-full focus:outline-none" ref="inputField" />
-                <div v-if="activeMessage?.length" @click="handleClearActiveMessage"
+
+                <div v-if="message?.length && !isLoading" @click="message = ''"
                     class="absolute top-0 right-0 flex items-center w-[30px] h-full">
-                    <CircleX />
+                    <CircleX class="size-4 text-gray-600" />
+                </div>
+
+                <div v-if="isLoading" class="absolute top-0 right-0 flex items-center w-[30px] h-full">
+                    <svg class="animate-spin size-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                        </path>
+                    </svg>
                 </div>
             </div>
 
@@ -24,16 +35,6 @@
                     </span>
                 </li>
             </ul>
-
-            <div v-if="isLoading" class="flex items-center bg-white px-2 shadow-md">
-                <svg class="animate-spin size-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24">
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path class="opacity-75" fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                    </path>
-                </svg>
-            </div>
 
             <!-- Button to send message -->
             <button :disabled="isQueryLoadingChat" @click="() => handleSendMessage(message, false)"
@@ -56,11 +57,10 @@
 </template>
 
 <script setup>
-import { CircleX } from 'lucide-vue-next'
 import { useItemsStore } from '@/stores/itemsStore';
 import debounce from 'lodash-es/debounce';
 import { capitalizeFirst, truncateString, normalizeQuery } from '../utils';
-import { History, TrendingUp, Sparkles } from 'lucide-vue-next';
+import { History, TrendingUp, Sparkles, CircleX } from 'lucide-vue-next';
 
 const itemsStore = useItemsStore()
 const { isQueryLoadingChat } = storeToRefs(itemsStore)
@@ -152,10 +152,17 @@ const scrollToBottom = () => {
 };
 
 const handleSetActiveQueryMessage = (query) => {
+    if (query === activeMessage.value) {
+        inputIsFocused.value = false;
+        return
+    }
+
     isSelectedManually.value = true;
 
     message.value = query;
     inputIsFocused.value = false;
+
+    handleSendMessage(query, false);
 
     setTimeout(() => {
         isSelectedManually.value = false;
