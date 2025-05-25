8f6b44cb4dad0371d5fa4f57c79b12fed1a1d363
Radu <radu.bordei89@gmail.com>
2025-05-25 22:02:34 +0300

    fomrat location with pretty

--- stores/location.js
+++ stores/location.js
@@ -1,22 +1,27 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
 
 export const useLocationStore = defineStore('location', () => {
-    const LOCAL_STORAGE_KEY = 'userLocation'
-    const location = process.client ? ref(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? '{}')) : ref({})
+  const LOCAL_STORAGE_KEY = 'userLocation'
+  const location = process.client
+    ? ref(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? '{}'))
+    : ref({})
 
-    const isLoading = ref(false)
+  const isLoading = ref(false)
 
-    const setLocationToStorage = (newVal) => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newVal))
-    }
+  const setLocationToStorage = (newVal) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newVal))
+  }
 
-    watch(() => location.value, (newVal) => {
-        setLocationToStorage(newVal)
-    })
+  watch(
+    () => location.value,
+    (newVal) => {
+      setLocationToStorage(newVal)
+    },
+  )
 
-    return {
-        setLocationToStorage,
-        isLoading,
-        location
-    }
-});
+  return {
+    setLocationToStorage,
+    isLoading,
+    location,
+  }
+})
