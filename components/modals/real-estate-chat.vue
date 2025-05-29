32f8a2926619a53b32a5fa73a032cf455fb4e502
Radu <radu.bordei89@gmail.com>
2025-05-29 20:06:08 +0300

    improves

--- components/modals/real-estate-chat.vue
+++ components/modals/real-estate-chat.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-full">
-        <Chat @submit="handleSendMessage" :isLoading="isQueryLoadingProperty" :messages="defaultThreadMessages">
+        <Chat-Property @submit="handleSendMessage" :isLoading="isQueryLoadingProperty" :messages="defaultThreadMessages">
             <template #header>
                 <div class="bg-blue-500 text-white px-4 rounded-tl-lg flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -12,8 +12,8 @@
             </template>
 
             <div>
-                <div v-if="!defaultThreadMessages.length" class="border-b border-gray-200 mx-auto flex justify-center">
-                    <nav class="-mb-px flex space-x-[12px]">
+                <div v-if="!defaultThreadMessages.length" :class="[tabs.length > 1 ? 'border-b border-gray-200' : '', 'mx-auto flex justify-center']">
+                    <nav class="-mb-px flex space-x-[12px]" v-if="tabs.length > 1">
                         <a v-for="(tab, index) in tabs.filter(el => el.visible)" :key="tab.title" class="cursor-pointer py-4 px-1 flex items-center" @click="selectTab(tab.slug)" :class="{ 'border-b border-gray-500 text-blue-500': activeTab === index }">
                             <span v-html="tab.icon" :class="[{'text-gray-800': tab.slug==activeTab, 'text-gray-500': tab.slug!=activeTab}, 'pr-2']"></span>
                             <span v-html="tab.title" :class="[{'text-gray-800 font-medium hover:text-gray-900': tab.slug==activeTab, 'text-gray-500 font-medium hover:text-gray-700': tab.slug!=activeTab}, 'whitespace-nowrap text-sm']"></span>
@@ -22,29 +22,26 @@
                 </div>
 
                 <div class="mx-auto flex flex-wrap justify-center my-2">
-                    <span v-for="(prompt, index) in activePrompts" @click="() => handleSelectPrompt(prompt)" :key="index" class="cursor-pointer text-center inline-flex items-center mt-2 rounded-md bg-gray-100 px-4 py-2 text-xs font-medium text-gray-600 mr-2">
+                    <span v-for="(prompt, index) in prompts" @click="() => handleSelectPrompt(prompt)" :key="index" class="cursor-pointer text-center inline-flex items-center mt-2 rounded-md bg-gray-100 px-4 py-2 text-xs font-medium text-gray-600 mr-2">
                         {{ prompt }}
                     </span>
                 </div>
 
-                <span @click="getActivePrompts" class="cursor-pointer flex items-center justify-center">
+                <span @click="() => triggerShuffle++" class="cursor-pointer flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 text-gray-500"><path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" /></svg>
                 </span>
             </div>
-        </Chat>
+        </Chat-Property>
     </div>
 </template>
 
 <script setup>
-import { useChatStore } from '@/stores/chat';
-import { useItemsStore } from '@/stores/itemsStore';
+import { usePropertyQuestionStore } from '~/stores/propertyQuestionStore';
+import { shuffleArray } from '../../utils';
 
-const chatStore = useChatStore()
-const itemsStore = useItemsStore();
+const propertyQuestionStore = usePropertyQuestionStore();
 
-const { isQueryLoadingProperty } = storeToRefs(itemsStore)
-
-const { sendMessage } = useMessagesService();
+const { loading: isQueryLoadingProperty } = storeToRefs(propertyQuestionStore)
 
 const props = defineProps({
     item: {
@@ -55,49 +52,34 @@ const props = defineProps({
 
 const emit = defineEmits(['select'])
 
+const transactionType = computed(() => props.item.transaction_type)
+const isForSale = computed(() => transactionType.value === 'for-sale')
+
 const tabs = ref([
     {
         title: "Sunt cumpărător",
         slug: 'buyer',
-        visible: props.item.transaction_type == 'for-sale',
+        visible: isForSale.value,
         icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-500"><path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>',
     },
-    {
-        title: "Sunt investor",
-        slug: 'investor',
-        visible: props.item.transaction_type == 'for-sale',
-        icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-500"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" /></svg>',
-    },
+    // {
+    //     title: "Sunt investor",
+    //     slug: 'investor',
+    //     visible: isForSale.value,
+    //     icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-500"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" /></svg>',
+    // },
 ]);
 
 const activeTab = ref('buyer');
 
-let activePrompts = ref([])
-const getActivePrompts = () => {
-  let promptsByThread = chatStore.handleGetPromptsByThread(activeTab.value);
-
-  // Function to shuffle the array
-  const shuffleArray = (arr) => {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
-  };
-
-  // Shuffle the prompts
-  let shuffledPrompts = shuffleArray([...promptsByThread]);
-
-  // Return the first 5 items (or fewer if there are less than 5 prompts)
-  activePrompts.value = shuffledPrompts.slice(0, 5);
-};
-
-onMounted(() => {
-    getActivePrompts()
-})
+const triggerShuffle = ref(0);
+const prompts = computed(() => {
+    triggerShuffle.value
+    return shuffleArray(propertyQuestionStore.handleGetPromptsByTab(activeTab.value)).slice(0, 5);
+});
 
 const defaultThreadMessages = computed(() => {
-  return chatStore.handleGetMessagesByThread(props.item.id)
+  return propertyQuestionStore.handleGetMessagesByPropertyId(props.item.id)
 })
 
 const handleSendMessage = async(message) => {
@@ -105,26 +87,12 @@ const handleSendMessage = async(message) => {
         let trimmedMessage = message.trim()
         if(! trimmedMessage) return
 
-        // adding user message to stack
-        chatStore.handlePushMessage(props.item.id, { text: message, sender: 'user' })
-
-        const { reply = null, item, intent, amenities } = await itemsStore.handleRequestDetails(props.item.id, trimmedMessage, {})
-        if(! item) throw new Error('No results found for' + trimmedMessage)
+        const { answer = null, meta: { intent, amenities, nearby_places } = {} } = await propertyQuestionStore.sendQuestion(props.item.id, trimmedMessage)
         
-        chatStore.handlePushMessage(props.item.id, { text: reply, sender: 'bot' })
-
         emit('select', amenities ? 'map' : 'general', amenities)
-
-        sendMessage(
-            props.item.id, props.item.id, trimmedMessage, 'user'
-        )
   } catch(err) {
-    chatStore.handlePushMessage(props.item.id, {
-      text: err,
-      sender: 'bot',
-    })
+    throw err
   }
-
 };
 
 const handleSelectPrompt = (prompt) => {
@@ -133,6 +101,5 @@ const handleSelectPrompt = (prompt) => {
 
 const selectTab = (slug) => {
     activeTab.value = slug;
-    getActivePrompts()
 }
 </script>
